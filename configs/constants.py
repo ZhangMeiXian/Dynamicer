bef73d3eb2f03d9d29420fbd0ecaeea67b9c1033
zhangmeixian <zhangmeixian@baidu.com>
2023-10-19 19:17:04 +0800

    add data process

---
+++ configs/constants.py
@@ -0,0 +1,15 @@
+# !/usr/bin/python3.8
+
+"""
+@ModuleName: 常量模块
+@author: zhangmeixian
+"""
+
+# global column names
+column_time = "time"
+column_label = "label"
+
+
+# global digits
+status_exception = 1
+status_normal = 0
